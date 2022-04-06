5e867eb096c90c8aabf4c3b2d1af9b861fd2ec0b
RayNawara <rjnawara@gmail.com>
2022-04-06 16:02:36 -0400

    Create turbo messages on create, update and destroy

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -34,7 +34,8 @@ class MessagesController < ApplicationController
           render turbo_stream: [
             turbo_stream.update('new_message', partial: "messages/form", locals: {message: Message.new}),
             turbo_stream.prepend('messages', partial: "messages/message", locals: {message: @message}),
-            turbo_stream.update('message_counter', Message.count)
+            turbo_stream.update('message_counter', Message.count),
+            turbo_stream.update('notice', "Message was successfully created.")
           ]
         end
         format.html { redirect_to message_url(@message), notice: "Message was successfully created." }
@@ -57,7 +58,8 @@ class MessagesController < ApplicationController
       if @message.update(message_params)
         format.turbo_stream do
           render turbo_stream: [
-            turbo_stream.update(@message, partial: "messages/message", locals: {message: @message})
+            turbo_stream.update(@message, partial: "messages/message", locals: {message: @message}),
+            turbo_stream.update('notice', "Message was successfully updated.")
           ]
         end
         format.html { redirect_to message_url(@message), notice: "Message was successfully updated." }
@@ -82,7 +84,8 @@ class MessagesController < ApplicationController
       format.turbo_stream do
         render turbo_stream: [
           turbo_stream.remove(@message),
-          turbo_stream.update('message_counter', Message.count)
+          turbo_stream.update('message_counter', Message.count),
+          turbo_stream.update('notice', "Message was successfully destroyed.")
       ]
       end
       format.html { redirect_to messages_url, notice: "Message was successfully destroyed." }
