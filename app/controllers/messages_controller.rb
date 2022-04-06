1b255dc1460d97595ee51777b5b2de4583e7144d
RayNawara <rjnawara@gmail.com>
2022-04-06 15:52:28 -0400

    Add counter for messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -33,7 +33,8 @@ class MessagesController < ApplicationController
         format.turbo_stream do
           render turbo_stream: [
             turbo_stream.update('new_message', partial: "messages/form", locals: {message: Message.new}),
-            turbo_stream.prepend('messages', partial: "messages/message", locals: {message: @message})
+            turbo_stream.prepend('messages', partial: "messages/message", locals: {message: @message}),
+            turbo_stream.update('message_counter', Message.count)
           ]
         end
         format.html { redirect_to message_url(@message), notice: "Message was successfully created." }
@@ -56,7 +57,7 @@ class MessagesController < ApplicationController
       if @message.update(message_params)
         format.turbo_stream do
           render turbo_stream: [
-            turbo_stream.update(@message, partial: "messages/message", locals: {message: Message.new})
+            turbo_stream.update(@message, partial: "messages/message", locals: {message: @message})
           ]
         end
         format.html { redirect_to message_url(@message), notice: "Message was successfully updated." }
@@ -78,7 +79,12 @@ class MessagesController < ApplicationController
     @message.destroy
 
     respond_to do |format|
-      format.turbo_stream  { render turbo_stream: turbo_stream.remove(@message) }
+      format.turbo_stream do
+        render turbo_stream: [
+          turbo_stream.remove(@message),
+          turbo_stream.update('message_counter', Message.count)
+      ]
+      end
       format.html { redirect_to messages_url, notice: "Message was successfully destroyed." }
       format.json { head :no_content }
     end
